63d374aac6efd6f28c82e0d966f561b3e7d15458
Anas-i-okasha <okasha.i.anas@gmail.com>
2020-11-20 21:50:08 +0200

    upload q1

--- Q01/main.js
+++ Q01/main.js
@@ -1,14 +1,10 @@
-console.log('Problem Solving Q: 1 ');
 
 /* Longest Word */
-
 /*  
-Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length.
+Have the function LongestWord(sen) take the sen parameter being passed and 
+return the largest word in the string. If there are two or more words that are the same length, 
+return the first word from the string with that length.
 */
-
-function longestWord(sen) {
-  // YOUR CODE HERE
-}
 
 /* 
 Examples:
@@ -17,3 +13,22 @@ longestWord('I love cats'); // => 'love'
 longestWord('coding is awesome'); // => 'awesome'
 longestWord('hello world'); // => 'hello'
 */
+
+// solving ==> //
+console.log('Answwer: Q1') 
+
+
+ longestWord=(sen)=>{
+  if(sen.length ===0){
+    return 'ther is no 0 word '
+  }
+  let output = '';
+  const words = sen.split(' ');
+  words.forEach((word)=>{
+    if(word.length > output.length){
+    output = word
+    }
+  })
+ return output;
+ 
+}
