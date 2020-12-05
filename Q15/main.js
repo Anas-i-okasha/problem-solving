03aa99d91ff93b029467c826e9b9206a13999273
Anas-i-okasha <okasha.i.anas@gmail.com>
2020-12-05 21:23:23 +0200

    upload q15

---
+++ Q15/main.js
@@ -0,0 +1,27 @@
+console.log('Problem Solving Q:15 ');
+
+/*  factorial*/
+
+/*  
+Given an integer, n, return n! (n factorial)
+5! = 5 * 4 * 3 * 2 * 1
+*/
+
+function factorial(number) {
+  let result = 1;
+  if (number == 0 || number == 1){
+    return result;
+  }else{
+    for(let i = number; i >= 1; i--){
+      result = result * i;
+    }
+    return result;
+  }  
+}
+
+/* 
+Examples:
+factorial(1) // => 1
+factorial(3) // => 6
+factorial(4) // => 24
+*/
