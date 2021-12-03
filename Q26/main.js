e702c5636af78138b227acbd629f2c778046b0f3
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-12-03 19:00:41 +0200

    solved two Q JS

--- Q26/main.js
+++ Q26/main.js
@@ -23,6 +23,37 @@ if(string[i]==ch){
 }
 return res.length
 }
-console.log(countCh("hello", "l"))  //=> 2
-console.log(countCh("hello", "e"))  // => 1
-console.log(countCh("hello", "z"))  // => 0
+// console.log(countCh("hello", "l"))  //=> 2
+// console.log(countCh("hello", "e"))  // => 1
+// console.log(countCh("hello", "z"))  // => 0
+
+// =========================================================================
+
+// Write a function `countWords` that accepts a string and returns an object that has all unique words as keys and the number of times the word was found in the string as a value
+
+function countsWord(word){
+    if(!word.length) return {}
+    let newWord={}
+    let res = word.split(" ")
+ res.forEach(element => {
+    if(!newWord[element]){
+        newWord[element]=1
+    }else{
+        newWord[element]++
+    }
+});
+return newWord
+}
+// console.log(countsWord("hello hello world"))  //{ hello: 2, world: 1 }
+// ============================================================================================================
+
+//Write a function `averageGrade` that accepts an array of student grades as values and returns the average grade and if the array is empty return "Please enter at least one grade".
+
+function avargeGr (grades){
+const result=grades.reduce((acc,ele)=>{
+return (acc + ele) 
+},0)
+return result / (grades.length)
+}
+console.log(avargeGr([100, 80, 95, 67, 74]))  // 83.2
+console.log(avargeGr([100, 90, 80, 70]))   // => 85
